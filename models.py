7e66fa7e70ff3877f64334b610705d752e314efc
Jorge CARDONA <jorgeecardona@gmail.com>
2010-07-01 11:47:23 -0500

    Changes on roles field

--- models.py
+++ models.py
@@ -1,25 +1,47 @@
 from django.db import models
+from django.utils import simplejson
 
 # Create your models here.
-from unique_model.models import UniqueModel
+#from unique_model.models import UniqueModel
 
-class Role(UniqueModel):
-    """
-    Role 
-    ====
-
-    """
-
+class ListStringField(models.TextField):
     
+    description = "A list of strings."
 
-    def has_perm(self, permission):
-        pass
+    __metaclass__ = models.SubfieldBase
+
+    def __init__(self, *args, **kwargs):
+        if not ('default' in kwargs):
+            kwargs['default'] = simplejson.dumps([])
+
+        super(ListStringField, self).__init__(*args, **kwargs)
     
+    def to_python(self, value):
+        if isinstance(value, list) and all([isinstance(x, (str, unicode)) for x in value]):
+            return value
 
-
-
-class RolesField(models.Field):
-    description="A list of roles"
-
-    def __init__(self, *args, **kwords):
+        value = simplejson.loads(value)
+        if isinstance(value, list) and all([isinstance(x, (str, unicode)) for x in value]):
+            return value
         
+        return []
+        
+    def get_prep_value(self, value):
+        return simplejson.dumps(value)
+
+class Agent(models.Model):
+    """
+    Agent
+    =====
+
+    This is an abstract class that define the minimum aspects 
+    to be an agent in Role-Based Access Control.
+
+    It define a basic list of roles, this roles are basically identified with the name of the role.
+    
+    """
+
+    class Meta(object):
+        abstract = True
+
+    roles = ListStringField()
