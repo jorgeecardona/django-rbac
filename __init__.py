91432e960a136912b367966ba005cce54510af08
Jorge CARDONA <jorgeecardona@gmail.com>
2010-07-04 15:16:37 -0500

    Added roles and identification/authentication/authorization process

--- __init__.py
+++ __init__.py
@@ -1,12 +1,36 @@
-from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 
 class RBACMiddleware(object):
     def process_request(self, request):
 
-        # Fetch user and profile.
-        if hasattr(request, 'user'):
-            if isinstance(request.user, User):
-                request.profile = request.user.get_profile()
-                
+        if '_rbac_subject' in request.session:
+            request.subject = request.session['_rbac_subject']
+        else:
+            request.subject = None
+
+def login_required(*args, **kwargs):
+    """
+    This function decorate a request in order to check if there is a user logged in.
+    """
+
+    def new_f(request, *args, **kwargs):
+        if '_rbac_subject' in request.session:
+            return f(request, *args, **kwargs)
+
+        return HttpResponseRedirect(redirect)
+
+    def dec(f):
+        return new_f
+
+    redirect = kwargs.get('redirect', '/accounts/login')
+
+    if (len(args) > 0) and callable(args[0]):
+        # Is a decorator without arguments.
+        f = args[0]
+        return new_f
+    
+    return dec
+
+
 
                 
--- models.py
+++ models.py
@@ -4,6 +4,50 @@ from django.utils import simplejson
 # Create your models here.
 #from unique_model.models import UniqueModel
 
+
+"""
+
+Role Based Access Control
+=========================
+
+This application build the essential models and function for a django site to
+use rbac.
+
+It defines several abtract models:
+ 
+ * Subject: A person or an automated agent.
+ * Role: Job function that define an authoritative level.
+ * Permission: An approval to access a resource in a particular mode.
+ * Session: A session mantain the information of Subject and Role. (django.contrib.sessions)
+
+
+ Subject
+ =======
+
+A subject is an user or an automated agent, as a node or a gadget. 
+This model defines two abstract methods:
+
+ * identify: Used to identify a subject given some identification parameters. A site has to define a particular subject model that inherite from Subject and define this function, with its identification parameters.
+
+ * authenticate: Used to authenticate a subject given some authentication parameters.
+
+ Role
+ ====
+
+A role is just a job function or a title that define a subsets of subjects. It helps to define the responsabilities of the subjects that belong to this role.
+
+ Permission
+ ==========
+
+
+ Resource
+ ========
+
+"""
+
+class NonImplementedException(Exception):
+    pass
+
 class ListStringField(models.TextField):
     
     description = "A list of strings."
@@ -29,10 +73,10 @@ class ListStringField(models.TextField):
     def get_prep_value(self, value):
         return simplejson.dumps(value)
 
-class Agent(models.Model):
+class Subject(models.Model):
     """
-    Agent
-    =====
+    Subject
+    =======
 
     This is an abstract class that define the minimum aspects 
     to be an agent in Role-Based Access Control.
@@ -45,3 +89,29 @@ class Agent(models.Model):
         abstract = True
 
     roles = ListStringField()
+
+    @classmethod
+    def identify(cls, *args, **kwargs):
+        """
+        This method should receive the parameters needed to identify an agent.
+
+        In a single case wher agents are regular human beigns an username or email is fine.
+        """
+        raise NonImplementedException("Define an identification mechanism.")
+    
+    def authenticate(self, *args, **kwargs):
+        raise NonImplementedException(
+            "Define an authentication mechanism, given an agent already identified.")
+        
+    def authorize(self, *args, **kwargs):
+        raise NonImplementedException(
+            "Define an authoization mechanism to perform transaction."
+            )
+
+    def authenticate_request(self, request, *args, **kwargs):
+        self.authenticate(*args, **kwargs)
+        request.session['_rbac_subject'] = self
+
+    def deauthenticate_request(self, request):
+        self.session.pop('_rbac_subject')
+        
